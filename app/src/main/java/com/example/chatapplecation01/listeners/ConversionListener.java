f9edee8b51c42428a4ffec4d39c16a13fca8b977
B200179B <b200179b@sc.edu.my>
2022-12-02 15:07:04 +0800

    First Commit

---
+++ app/src/main/java/com/example/chatapplecation01/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.example.chatapplecation01.listeners;
+
+import com.example.chatapplecation01.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/example/chatapplecation01/listeners/UserListener.java
@@ -0,0 +1,8 @@
+package com.example.chatapplecation01.listeners;
+
+import com.example.chatapplecation01.models.User;
+
+public interface UserListener {
+        void onUserClicked(User user);
+
+}
